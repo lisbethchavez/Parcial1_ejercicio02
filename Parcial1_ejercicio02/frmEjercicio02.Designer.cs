ca85e6acb202a2d9db91aa6775791a8a6e86a9a9
estudiante <estudiante@L-P1A-116>
2021-12-06 12:12:31 -0500

    Agregar archivos de proyecto.

---
+++ Parcial1_ejercicio02/frmEjercicio02.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Parcial1_ejercicio02
+{
+    partial class frmEjercicio02
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstPalabras = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa la frase";
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.Location = new System.Drawing.Point(149, 19);
+            this.txtFrase.Multiline = true;
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(320, 62);
+            this.txtFrase.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(501, 22);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(86, 28);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Resultado";
+            // 
+            // lstPalabras
+            // 
+            this.lstPalabras.FormattingEnabled = true;
+            this.lstPalabras.Location = new System.Drawing.Point(25, 144);
+            this.lstPalabras.Name = "lstPalabras";
+            this.lstPalabras.Size = new System.Drawing.Size(240, 95);
+            this.lstPalabras.TabIndex = 4;
+            // 
+            // frmEjercicio02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 303);
+            this.Controls.Add(this.lstPalabras);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEjercicio02";
+            this.Text = "CUENTA PALABRAS CON SU RESPECTIVA LONGITUD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstPalabras;
+    }
+}
+
---
+++ Parcial1_ejercicio02/frmEjercicio02.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1_ejercicio02
+{
+    public partial class frmEjercicio02 : Form
+    {
+        public frmEjercicio02()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //valido si el usuario ingreso una frase
+
+            if(this.txtFrase.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor ingresa una frase...");
+                //Ubica el cursor en el cuadro de texto txtFrase
+                this.txtFrase.Focus();
+            }
+
+            //creo el array de palabras
+
+            string[] palabras = this.txtFrase.Text.Split(' ');
+            this.lstPalabras.Items.Clear();
+
+            //lleno el listbox con las palabras
+
+            for(int i=0; i<palabras.Length;i++)
+            {
+                if(palabras[i].Length > 0)
+                this.lstPalabras.Items.Add(palabras[i] + "-->" + palabras[i].Length+"caracteres...");
+
+            }
+
+
+        }
+    }
+}
